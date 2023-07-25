634ee858ac9898d03fa4741c1090fb1705126130
ssh-22 <ryosuke.masuda@beproud.jp>
2023-07-25 17:16:22 +0900

    refactoring

--- main.go
+++ main.go
@@ -7,107 +7,87 @@ import (
 	"strings"
 )
 
-var SWITCH_HOST_IP string
-
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Please provide a command: local or status")
+		fmt.Println("Please provide a command: switch or status")
 		os.Exit(1)
 	}
 
 	command := os.Args[1]
 
-	SWITCH_HOST_IP = os.Getenv("SWITCH_HOST_IP")
-	if SWITCH_HOST_IP == "" {
+	switchHostIP := os.Getenv("SWITCH_HOST_IP")
+	if switchHostIP == "" {
 		fmt.Println("SWITCH_HOST_IP environment variable is not set.")
 		os.Exit(1)
 	}
 
 	switch command {
 	case "switch":
-		switchEnv()
+		if err := switchEnv(switchHostIP); err != nil {
+			fmt.Println("Failed to switch environment:", err)
+			os.Exit(1)
+		}
 	case "status":
-		printStatus()
+		status, err := getStatus(switchHostIP)
+		if err != nil {
+			fmt.Println("Failed to get status:", err)
+			os.Exit(1)
+		}
+		fmt.Println(status)
 	default:
 		fmt.Println("Invalid command. Valid commands are: switch, status")
 		os.Exit(1)
 	}
 }
 
-func readHostsFile() ([]string, error) {
+func readHostsFile() (string, error) {
 	hosts, err := ioutil.ReadFile("/private/etc/hosts")
-	if err != nil {
-		fmt.Println("Error opening hosts file:", err)
-		os.Exit(1)
-	}
-
-	lines := strings.Split(string(hosts), "\n")
-	return lines, err
+	return string(hosts), err
 }
 
-func switchEnv() error {
+func switchEnv(switchHostIP string) error {
+	hosts, err := readHostsFile()
+	if err != nil {
+		return fmt.Errorf("Error opening hosts file: %w", err)
+	}
 
-	lines, err := readHostsFile()
-
+	lines := strings.Split(hosts, "\n")
 	var hasSwitchHost = false
-
 	for i, line := range lines {
-		{
-			if strings.HasPrefix(line, "# "+SWITCH_HOST_IP) {
-				lines[i] = strings.TrimPrefix(line, "# ")
-				hasSwitchHost = true
-				fmt.Println("Successfully switching to local")
-				break
-			} else if strings.HasPrefix(line, SWITCH_HOST_IP) {
-				lines[i] = "# " + line
-				hasSwitchHost = true
-				fmt.Println("Successfully switching to dev")
-				break
-			}
+		if strings.HasPrefix(line, "# "+switchHostIP) {
+			lines[i] = strings.TrimPrefix(line, "# ")
+			hasSwitchHost = true
+			fmt.Println("Successfully switching to local")
+			break
+		} else if strings.HasPrefix(line, switchHostIP) {
+			lines[i] = "# " + line
+			hasSwitchHost = true
+			fmt.Println("Successfully switching to dev")
+			break
 		}
 	}
 
 	if !hasSwitchHost {
-		fmt.Println("Error cannot find SWITCH_HOST_IP in hosts file:", SWITCH_HOST_IP)
-		os.Exit(1)
+		return fmt.Errorf("Error cannot find SWITCH_HOST_IP in hosts file: %s", switchHostIP)
 	}
 
-	err = ioutil.WriteFile("/private/etc/hosts", []byte(strings.Join(lines, "\n")), 0644)
-	if err != nil {
-		fmt.Println("Error writing to hosts file:", err)
-		os.Exit(1)
-	}
-	return err
+	return ioutil.WriteFile("/private/etc/hosts", []byte(strings.Join(lines, "\n")), 0644)
 }
 
-func printStatus() {
-	status, err := getStatus()
+func getStatus(switchHostIP string) (string, error) {
+	hosts, err := readHostsFile()
 	if err != nil {
-		fmt.Println("Error calling getStatus:", err)
-		os.Exit(1)
-	}
-	fmt.Println(status)
-}
-
-func getStatus() (string, error) {
-	lines, err := readHostsFile()
-
-	if err != nil {
-		fmt.Println("Error reading hosts file:", err)
-		os.Exit(1)
+		return "", fmt.Errorf("Error reading hosts file: %w", err)
 	}
 
+	lines := strings.Split(hosts, "\n")
 	for _, line := range lines {
-		{
-			if strings.HasPrefix(line, "# "+SWITCH_HOST_IP) {
-				return "dev", err
-			} else if strings.HasPrefix(line, SWITCH_HOST_IP) {
-				return "local", err
-			}
+		if strings.HasPrefix(line, "# "+switchHostIP) {
+			return "dev", nil
+		} else if strings.HasPrefix(line, switchHostIP) {
+			return "local", nil
 		}
 	}
 
-	fmt.Println("Error cannot find SWITCH_HOST_IP in hosts file:", SWITCH_HOST_IP)
-	os.Exit(1)
-	return "", err
+	return "", fmt.Errorf("Error cannot find SWITCH_HOST_IP in hosts file: %s", switchHostIP)
 }
